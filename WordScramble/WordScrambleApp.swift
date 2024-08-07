99bffbe5b22662910109e4a607e7c196280b31e7
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-08-06 23:42:16 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Marcello Gonzatto Birkan on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
